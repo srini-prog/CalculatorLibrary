6714977a353526f0e07901e1bb7a397a44c7522b
srini-prog <84773885+srini-prog@users.noreply.github.com>
2021-05-26 07:35:24 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -7,6 +7,4 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
-cd CalculatorLibrary
-git add calculator.py
-git commit -m "Add functions for addition and subtraction"
+
